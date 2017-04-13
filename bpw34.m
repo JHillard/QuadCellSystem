56cb62149cd52d8bb2727f7f9bba1b223ff2fdf3
Jake Hillard <jhillard3.14@gmail.com>
2017-04-13 15:54:14 -0700

    Converted some jupyter code to matlab. Verified opamp models for opa111 in both inoise and vnoise. Todo; Get full TIA noise sim. Add other blocks.

---
+++ bpw34.m
@@ -0,0 +1,7 @@
+function specs = bpw34()
+    %photodiode is BPW34
+    %http://www.vishay.com/docs/81521/bpw34.pdf
+    PHOTODIODE_RESPONSIVITY = 10E-6/(0.2)*0.001/(0.01.^2) %Grabbed from plot...
+    PHOTODIODE_CAPACITANCE = 40E-12
+    specs = {PHOTODIODE_RESPONSIVITY, PHOTODIODE_CAPACITANCE};
+end
