b195914fa40c4bc7e6601838e36e2009770c72d9
Jake Hillard <jhillard3.14@gmail.com>
2017-04-16 21:30:04 -0700

    Added Atmospheric models based on OPALs and MODTRAN data. Todo; fix geometeric losses and check light recieved from MEO.

--- link_block.m
+++ link_block.m
@@ -25,9 +25,19 @@ function output_package= link_block(sigs, orbit_package)
     %Weirdly, getting different answers for ideal transmission
     
     %Atmosphereic Transfer Function for Laser
+    %This should be simulated with MODTRAN, a $1500 yearly piece of
+    %software. I've interpolated the attenuation graph from OPALS "Optical
+    %Payload for Lasercom..." Fig 2.b.
     air_mass = 1./( cos(zenith_ang) + 0.50572.*(6.07995 + 90-zenith_ang).^-1.6364);
-    
-    
+    %^Uses Kasten and Young Model to calculate air_mass. See Opals Paper
+    %eq(1). section 2.1
+    attenuation = rural_23km_cloudy_model(air_mass);
+    %Rural_20km fit Opal's mission the best. 
+    atmo_signal = geometeric_signal.*db2mag(attenuation);
+    if(verbose)
+        'Atmospheric Attenuation'
+        attenuation
+    end
     %output_package = {geometeric_signal, output_noise};
 end
 
--- tester.m
+++ tester.m
@@ -1,4 +1,15 @@
 sigs= {ones(100,1)', zeros(1,100)};
 orbit_package = {0,430};
 
-a = link_block(sigs, orbit_package);
+a = linspace(1,5.5);
+global verbose
+verbose = 0
+figure
+hold on
+axis([0 6 -15 0]);
+plot(a, desert_ext_model(a));
+plot(a, rural_5km_cloudy_model(a));
+plot(a, rural_23km_cloudy_model(a));
+plot(a, rural_23km_NC_model(a));
+
+
